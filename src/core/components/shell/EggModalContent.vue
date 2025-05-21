960e10d0f5f5bd9117f3b48d20a43cf98846803e
Mario Vellandi <mvellandi@hotmail.com>
2025-05-21 14:38:18 +0200

    Refactor modal styles by moving them from core.css to EggModalContent.vue

--- src/core/components/shell/EggModalContent.vue
+++ src/core/components/shell/EggModalContent.vue
@@ -19,4 +19,39 @@ const props = defineProps<{
 }>();
 </script>
 
-<style scoped></style>
+<style scoped>
+.ee-content {
+  position: relative;
+  overflow-y: auto;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+  opacity: 0;
+  transition: all 0.3s ease-in-out;
+  pointer-events: none;
+  padding: 2rem;
+  width: 100%;
+  max-width: 80vw;
+  min-height: auto;
+  max-height: calc(100vh - 10rem); /* leave room for padding */
+
+  @media (min-width: 640px) {
+    max-width: 80vw;
+    max-height: calc(100vh - 6rem);
+    overflow-y: auto;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 80vw;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 70vw;
+  }
+}
+
+.ee-content.ee-content-visible {
+  transform: scale(1);
+  opacity: 1;
+  pointer-events: auto;
+}
+</style>
