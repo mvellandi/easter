526bbe6a7b8f54d893d2c78a9acff0d155f33aaa
Mario Vellandi <mvellandi@hotmail.com>
2025-05-22 11:00:52 +0200

    unneeded

--- src/eggs/hello-world/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Component } from 'vue';
-import HelloWorldEgg from "./HelloWorldEgg.vue";
-
-export const HelloWorldEggComponent: Component = HelloWorldEgg;
-
-export default {
-  HelloWorldEggComponent,
-};
