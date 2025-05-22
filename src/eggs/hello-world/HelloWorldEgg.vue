62e9827fb68da2df8b4ead46b50e15c96a76e9c4
Mario Vellandi <mvellandi@hotmail.com>
2025-05-22 10:22:51 +0200

    Reduce success image display duration in ControllerRemote and simplify modal content in HelloWorldEgg.

--- src/eggs/hello-world/HelloWorldEgg.vue
+++ src/eggs/hello-world/HelloWorldEgg.vue
@@ -4,10 +4,7 @@
     <div class="space-y-2">
       <p>This is a standard modal.</p>
       <p>The shell is a Vue SFC managed by a core UI system.</p>
-      <p>
-        The content is a Vue SFC that can be independently developed and
-        registered
-      </p>
+      <p>The content is a Vue SFC that can be independently developed.</p>
     </div>
   </div>
 </template>
